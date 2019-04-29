d6abb525714254463891f7ff4080f3a7ff16c096
8176135 <8176135@gmail.com>
2019-04-30 10:43:10 +1200

    Reverted tabs back to spaces to fit with upstream

--- src/chit/details/alternatives/mod.rs
+++ src/chit/details/alternatives/mod.rs
@@ -5,95 +5,95 @@ use reqwest::StatusCode;
 
 #[derive(Serialize, Deserialize)]
 pub struct AlternativesSet {
-	pub name: String,
-	pub alternatives: Vec<String>,
+    pub name: String,
+    pub alternatives: Vec<String>,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct Alternatives {
-	pub description: String,
-	pub sets: Vec<AlternativesSet>,
+    pub description: String,
+    pub sets: Vec<AlternativesSet>,
 }
 
 #[derive(Debug)]
 pub enum RetrieveAlternativesError {
-	ParseError(serde_json::Error),
-	ConnectionError(reqwest::Error),
-	ReadError(std::io::Error),
+    ParseError(serde_json::Error),
+    ConnectionError(reqwest::Error),
+    ReadError(std::io::Error),
 }
 
 impl fmt::Display for RetrieveAlternativesError {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		match self {
-			RetrieveAlternativesError::ParseError(err) => write!(f, "Parse error: {}", err),
-			RetrieveAlternativesError::ReadError(err) => write!(f, "Read error: {}", err),
-			RetrieveAlternativesError::ConnectionError(err) => write!(f, "Connection error: {}", err),
-		}
-	}
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            RetrieveAlternativesError::ParseError(err) => write!(f, "Parse error: {}", err),
+            RetrieveAlternativesError::ReadError(err) => write!(f, "Read error: {}", err),
+            RetrieveAlternativesError::ConnectionError(err) => write!(f, "Connection error: {}", err),
+        }
+    }
 }
 
 
 // This is important for other errors to wrap this one.
 impl Error for RetrieveAlternativesError {
-	fn description(&self) -> &str {
-		match self {
-			RetrieveAlternativesError::ParseError(err) => err.description(),
-			RetrieveAlternativesError::ReadError(err) => err.description(),
-			RetrieveAlternativesError::ConnectionError(err) => err.description(),
-		}
-	}
+    fn description(&self) -> &str {
+        match self {
+            RetrieveAlternativesError::ParseError(err) => err.description(),
+            RetrieveAlternativesError::ReadError(err) => err.description(),
+            RetrieveAlternativesError::ConnectionError(err) => err.description(),
+        }
+    }
 
-	fn cause(&self) -> Option<&Error> {
-		// Generic error, underlying cause isn't tracked.
-		Some(match self {
-			RetrieveAlternativesError::ParseError(err) => err,
-			RetrieveAlternativesError::ReadError(err) => err,
-			RetrieveAlternativesError::ConnectionError(err) => err,
-		})
-	}
+    fn cause(&self) -> Option<&Error> {
+        // Generic error, underlying cause isn't tracked.
+        Some(match self {
+            RetrieveAlternativesError::ParseError(err) => err,
+            RetrieveAlternativesError::ReadError(err) => err,
+            RetrieveAlternativesError::ConnectionError(err) => err,
+        })
+    }
 }
 
 impl From<reqwest::Error> for RetrieveAlternativesError {
-	fn from(item: reqwest::Error) -> Self {
-		RetrieveAlternativesError::ConnectionError(item)
-	}
+    fn from(item: reqwest::Error) -> Self {
+        RetrieveAlternativesError::ConnectionError(item)
+    }
 }
 
 impl From<serde_json::Error> for RetrieveAlternativesError {
-	fn from(item: serde_json::Error) -> Self {
-		RetrieveAlternativesError::ParseError(item)
-	}
+    fn from(item: serde_json::Error) -> Self {
+        RetrieveAlternativesError::ParseError(item)
+    }
 }
 
 impl From<std::io::Error> for RetrieveAlternativesError {
-	fn from(item: std::io::Error) -> Self {
-		RetrieveAlternativesError::ReadError(item)
-	}
+    fn from(item: std::io::Error) -> Self {
+        RetrieveAlternativesError::ReadError(item)
+    }
 }
 
 const ALTERNATIVES_URL: &str =
-	"https://raw.githubusercontent.com/peterheesterman/chit/master/alternatives.json";
+    "https://raw.githubusercontent.com/peterheesterman/chit/master/alternatives.json";
 
 pub fn get_alternatives() -> Result<Alternatives, RetrieveAlternativesError> {
-	let mut res = reqwest::get(ALTERNATIVES_URL)?;
+    let mut res = reqwest::get(ALTERNATIVES_URL)?;
 
-	let mut res_body = String::new();
+    let mut res_body = String::new();
 
-	res.read_to_string(&mut res_body)?;
+    res.read_to_string(&mut res_body)?;
 
-	let alternatives: Alternatives =
-		serde_json::from_str(&res_body)?;
+    let alternatives: Alternatives =
+        serde_json::from_str(&res_body)?;
 
-	Ok(alternatives)
+    Ok(alternatives)
 }
 
 #[cfg(test)]
 mod tests {
-	use super::*;
+    use super::*;
 
-	#[test]
-	fn can_parse_alternatives() {
-		let alternatives = get_alternatives().expect("Alternative retrieval failed");
-		assert_ne!(alternatives.sets.len(), 0)
-	}
+    #[test]
+    fn can_parse_alternatives() {
+        let alternatives = get_alternatives().expect("Alternative retrieval failed");
+        assert_ne!(alternatives.sets.len(), 0)
+    }
 }
--- src/chit/details/mod.rs
+++ src/chit/details/mod.rs
@@ -7,135 +7,135 @@ use super::format;
 mod alternatives;
 
 pub fn print_details(crate_name: String) {
-	let mut width = format::get_width();
-	println!("{} {}...", "Searching for".magenta(), &crate_name.blue());
+    let mut width = format::get_width();
+    println!("{} {}...", "Searching for".magenta(), &crate_name.blue());
 
-	match crates::get(crates::url(&crate_name)) {
-		Some(crate_json) => {
-			if let Some(fields) = extract::crate_fields(crate_json) {
-				// Asume repository is the longest field
-				let repository_details = format!("Repository: {}", &fields.repository);
+    match crates::get(crates::url(&crate_name)) {
+        Some(crate_json) => {
+            if let Some(fields) = extract::crate_fields(crate_json) {
+                // Asume repository is the longest field
+                let repository_details = format!("Repository: {}", &fields.repository);
 
-				let keywords = format!("Keywords: {}", fields.keywords.join(", "));
-				let categories = format!("Categories: {}", fields.categories.join(", "));
+                let keywords = format!("Keywords: {}", fields.keywords.join(", "));
+                let categories = format!("Categories: {}", fields.categories.join(", "));
 
-				let large_widths: Vec<usize> = vec![
-					width,
-					repository_details.len(),
-					keywords.len(),
-					categories.len(),
-				];
+                let large_widths: Vec<usize> = vec![
+                    width,
+                    repository_details.len(),
+                    keywords.len(),
+                    categories.len(),
+                ];
 
-				width = *large_widths.iter().max().unwrap();
+                width = *large_widths.iter().max().unwrap();
 
-				println!("{}", format::title_bar(width, &crate_name));
+                println!("{}", format::title_bar(width, &crate_name));
 
-				// Description
-				format::bounded_print(width, &fields.description);
+                // Description
+                format::bounded_print(width, &fields.description);
 
-				println!("");
-				// Rating
-				let rating = extract::calculate_rating(&fields);
-				format::print_rating(rating);
+                println!("");
+                // Rating
+                let rating = extract::calculate_rating(&fields);
+                format::print_rating(rating);
 
-				// Download count
-				if let Some(download_count) = fields.downloads {
-					format::print(format!("Total downloads: {}", download_count));
-				}
+                // Download count
+                if let Some(download_count) = fields.downloads {
+                    format::print(format!("Total downloads: {}", download_count));
+                }
 
-				if let Some(recent_downloads) = fields.recent_downloads {
-					format::print(format!("Recent downloads: {}", recent_downloads));
-				}
+                if let Some(recent_downloads) = fields.recent_downloads {
+                    format::print(format!("Recent downloads: {}", recent_downloads));
+                }
 
-				let recent_version = &fields.versions[0];
-				format::print(format!(
-					"Latest version: {} ({})",
-					recent_version.semver, recent_version.date
-				));
+                let recent_version = &fields.versions[0];
+                format::print(format!(
+                    "Latest version: {} ({})",
+                    recent_version.semver, recent_version.date
+                ));
 
-				format::print(format!("Docs: {}", fields.documentation));
+                format::print(format!("Docs: {}", fields.documentation));
 
-				format::print(repository_details);
+                format::print(repository_details);
 
-				format::print(format!(
-					"Crates.io: https://crates.io/crates/{}",
-					&crate_name
-				));
+                format::print(format!(
+                    "Crates.io: https://crates.io/crates/{}",
+                    &crate_name
+                ));
 
-				format::print(format!("License: {}", recent_version.license));
+                format::print(format!("License: {}", recent_version.license));
 
-				if let Some(size) = recent_version.size_in_bytes {
-					format::print(format!(
-						"Crate size: {} kB",
-						(size as f64 / 1000_f64).round()
-					));
-				}
+                if let Some(size) = recent_version.size_in_bytes {
+                    format::print(format!(
+                        "Crate size: {} kB",
+                        (size as f64 / 1000_f64).round()
+                    ));
+                }
 
-				// TODO: move to another file
-				// ---------------------------------------------------------
-				// TODO: Clean this up by making it less imparative
-				let mut found_alternative = false;
-				let alternatives = alternatives::get_alternatives();
+                // TODO: move to another file
+                // ---------------------------------------------------------
+                // TODO: Clean this up by making it less imparative
+                let mut found_alternative = false;
+                let alternatives = alternatives::get_alternatives();
 
-				match alternatives {
-					Ok(alternatives) => {
-						for i in 0..alternatives.sets.len() {
-							let set = &alternatives.sets[i];
+                match alternatives {
+                    Ok(alternatives) => {
+                        for i in 0..alternatives.sets.len() {
+                            let set = &alternatives.sets[i];
 
-							if !set.alternatives.iter().any(|x| x == &crate_name) {
-								continue;
-							}
+                            if !set.alternatives.iter().any(|x| x == &crate_name) {
+                                continue;
+                            }
 
-							let mut alternatives = set.alternatives.clone();
-							alternatives.retain(|x| *x != crate_name);
-							let list_line = format!("Alternatives: {}", alternatives.join(", "));
-							format::bounded_print(width, &list_line);
-							found_alternative = true;
-						}
+                            let mut alternatives = set.alternatives.clone();
+                            alternatives.retain(|x| *x != crate_name);
+                            let list_line = format!("Alternatives: {}", alternatives.join(", "));
+                            format::bounded_print(width, &list_line);
+                            found_alternative = true;
+                        }
 
-						if !found_alternative {
-							format::print("Alternatives: None listed - Know one? Make a PR!".to_string());
-						}
-					}
-					Err(err) => {
-						format::print(format!("Error retrieving alternatives because: {}", err));
-					}
-				}
+                        if !found_alternative {
+                            format::print("Alternatives: None listed - Know one? Make a PR!".to_string());
+                        }
+                    }
+                    Err(err) => {
+                        format::print(format!("Error retrieving alternatives because: {}", err));
+                    }
+                }
 
-				// ---------------------------------------------------------
+                // ---------------------------------------------------------
 
-				format::print(keywords);
-				format::print(categories);
-			}
-		}
-		None => {
-			println!("{} {}", "Failed".red(), "to find that crate".magenta());
-			return;
-		}
-	}
+                format::print(keywords);
+                format::print(categories);
+            }
+        }
+        None => {
+            println!("{} {}", "Failed".red(), "to find that crate".magenta());
+            return;
+        }
+    }
 
-	match crates::get(crates::owners_url(&crate_name)) {
-		Some(crate_owners_json) => {
-			let owners_names: Vec<String> = crate_owners_json["users"]
-				.as_array()
-				.unwrap()
-				.iter()
-				.filter(|json| json["kind"] == "user")
-				.map(|json| format::remove_quotes(json["name"].to_string()))
-				.collect();
-			let multiple = owners_names.len() > 1;
+    match crates::get(crates::owners_url(&crate_name)) {
+        Some(crate_owners_json) => {
+            let owners_names: Vec<String> = crate_owners_json["users"]
+                .as_array()
+                .unwrap()
+                .iter()
+                .filter(|json| json["kind"] == "user")
+                .map(|json| format::remove_quotes(json["name"].to_string()))
+                .collect();
+            let multiple = owners_names.len() > 1;
 
-			let owners_names = owners_names.join(", ");
+            let owners_names = owners_names.join(", ");
 
-			// Owners
-			format::print(format!(
-				"Owner{}: {}",
-				if multiple { "s" } else { "" },
-				owners_names
-			));
-		}
-		None => println!("Failed to get crate owner details"),
-	}
+            // Owners
+            format::print(format!(
+                "Owner{}: {}",
+                if multiple { "s" } else { "" },
+                owners_names
+            ));
+        }
+        None => println!("Failed to get crate owner details"),
+    }
 
-	println!("{}", format::end_bar(width));
+    println!("{}", format::end_bar(width));
 }
